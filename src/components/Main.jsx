cccec97a4ed60688650839cb066ef586704a90f2
Lloyd Eddy <lloyd@lloyds-mbp.home>
2021-10-25 16:32:14 -0400

    final

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,41 @@
+import { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import Index from "../pages/Index"
 import Show from "../pages/Show"
 
 const Main = (props) => {
+    const [ people, setPeople ] = useState(null);
+
+    const URL = 'http://localhost:3001/people/';
+
+    const getPeople = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setPeople(data);
+    };
+
+    const createPeople = async (person) => {
+//make post reqeust to create people
+        await fetch(URL, {
+            method: "POST", 
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify(person),
+        });
+        // update list of people
+        getPeople();
+    };
+//make sure to get people when app loads
+// in other words we need a side-effect to occur as a result of the page
+// loading. We will use teh useEffect fnctn run on page load
+    useEffect(() => getPeople(), []);
 
     return (
         <main>
             <Switch>
                 <Route exact path="/">
-                    <Index />
+                    <Index people={people} createPeople={createPeople}/>
                 </Route>
                 <Route
                 path="/people/:id"
